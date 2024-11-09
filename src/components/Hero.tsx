6b18c2024e6fc27386ade34ae14adf94496122a6
Qharny <cannycc1020@gmail.com>
2024-11-09 02:43:51 +0000

    added particles js

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,21 +4,126 @@
 import Link from 'next/link';
 import {FaTwitter, FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 import Image from 'next/image';
-// import dynamic from 'next/dynamic';
+import Particles from 'react-tsparticles'; 
 
-// Dynamically import the 3D model component with no SSR
-// const ThreeDModel = dynamic(() => import('./3DModel'), {
-//   ssr: false,
-//   loading: () => (
-//     <div className="flex items-center justify-center w-[500px] h-[600px]">
-//       <div className="w-32 h-32 border-b-2 border-blue-600 rounded-full animate-spin" />
-//     </div>
-//   )
-// });
 
 const Hero = () => {
     return (
         <div className="bg-[#E9E5F4] min-h-screen relative px-4 sm:px-8 py-8 sm:py-12">
+            {/* Particles Background */}
+            <Particles 
+                options={{
+                    particles: {
+                        number: {
+                            value: 50,
+                            density: {
+                                enable: true,
+                                value_area: 800
+                            }
+                        },
+                        color: {
+                            value: "#000000" // Particle color
+                        },
+                        shape: {
+                            type: "circle", // Shape of particles
+                            stroke: {
+                                width: 0,
+                                color: "#000000"
+                            },
+                            polygon: {
+                                nb_sides: 5 // Number of sides for polygon shapes
+                            },
+                            image: {
+                                src: "/vercel.svg", // Image for particles
+                                width: 100,
+                                height: 100
+                            }
+                        },
+                        opacity: {
+                            value: 0.5, // Opacity of particles
+                            random: false,
+                            anim: {
+                                enable: false,
+                                speed: 1,
+                                opacity_min: 0.1,
+                                sync: false
+                            }
+                        },
+                        size: {
+                            value: 5, // Size of particles
+                            random: true,
+                            anim: {
+                                enable: false,
+                                speed: 40,
+                                size_min: 0.1,
+                                sync: false
+                            }
+                        },
+                        line_linked: {
+                            enable: true,
+                            distance: 150,
+                            color: "#000000",
+                            opacity: 0.4,
+                            width: 1
+                        },
+                        move: {
+                            enable: true,
+                            speed: 6,
+                            direction: "none",
+                            random: false,
+                            straight: false,
+                            out_mode: "out",
+                            bounce: false,
+                            attract: {
+                                enable: false,
+                                rotateX: 600,
+                                rotateY: 1200
+                            }
+                        }
+                    },
+                    interactivity: {
+                        detect_on: "canvas",
+                        events: {
+                            onhover: {
+                                enable: true,
+                                mode: "repulse"
+                            },
+                            onclick: {
+                                enable: true,
+                                mode: "push"
+                            },
+                            resize: true
+                        },
+                        modes: {
+                            grab: {
+                                distance: 400,
+                                line_linked: {
+                                    opacity: 1
+                                }
+                            },
+                            bubble: {
+                                distance: 400,
+                                size: 40,
+                                duration: 2,
+                                opacity: 8,
+                                speed: 3
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4
+                            },
+                            push: {
+                                particles_nb: 4
+                            },
+                            remove: {
+                                particles_nb: 2
+                            }
+                        }
+                    },
+                    retina_detect: true // Enable retina support
+                }} 
+                className="absolute top-0 left-0 w-full h-full z-0" // Add particles as background
+            />
 
             {/* Left Section - Name */}
             <div className="relative z-10 mb-8 text-center lg:absolute lg:left-20 lg:top-1/2 lg:-translate-y-1/2 lg:text-left lg:mb-0">
